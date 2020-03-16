e52a3a71c18e0a23aefc6c2741905bfcf2e39cab
Amir Sinanovic <asinanovic@Midland.Optical>
2020-03-15 19:14:36 -0500

    this time

--- js/note.js
+++ js/note.js
@@ -2,15 +2,15 @@ $(function(){
     var notesList = [];
     var selectedID = 0;
 
-    $("#btnSubmit").on("click"), function(){
+    $("btnSubmit").on("click"), function(){
         var note = new Object();
-        note.title = $("#title").val();
-        note.content = $("#note").val();
+        note.title = $("title").val();
+        note.content = $("note").val();
         notesList.push(note);
     };
-    $("#display.htm").on("pagebeforeshow", function(){
+    $("display.htm").on("pagebeforeshow", function(){
         for (i=0; i<notesList.length; i++){
-            $("#list_notes").append("<li id='" +  i + "'><a href='page_viewNote'>" + notesList[i].title + "</a></li>");
+            $("list_notes").append("<li id='" +  i + "'><a href='page_viewNote'>" + notesList[i].title + "</a></li>");
         }
     });
 });
